a2a4c6eda3283961b8a677b8743c1aa27aa30f63
Andrii Kurdiumov <kant2002@gmail.com>
2019-01-12 18:05:49 +0600

    Add Client side blazor on Razor Components

---
+++ rzr.App/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.AspNetCore.Components.Hosting;
+
+namespace rzr.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
